946298298907062b9ccdca3e98fb1d0b11259649
LeahDaniel <leah.daniel1997@gmail.com>
2021-12-31 20:13:07 -0600

    sass installed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter } from "react-router-dom"
 import { Trove } from "./components/Trove"
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css'
+import './scss/custom.scss'
 import "./index.css"
 
 ReactDOM.render(
